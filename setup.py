02575d2f8b92be905da0b39e4bd22499490f6f74
mitsuo <mitsuo_h@outlook.com>
2017-09-20 19:06:33 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='rangedl',
       url='http://github.com/johejo/rangedl.git',
       packages=find_packages(),
       entry_points={
-          'console_scripts': 'rangedl = http_download:main'
+          'console_scripts': 'rangedl = rangedl.http_download:main'
       },
       install_requires=['tqdm>=4.15.0',
                         'requests>=2.14.2']
